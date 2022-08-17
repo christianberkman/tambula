91a2f519eea3da5fb45fa8c96413805870af2645
Christian Berkman <christianberkman@gmail.com>
2022-08-17 20:53:00 +0300

    Created debug page

---
+++ debug.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Tambula Simple Redirect Class
+ * Christian Berkman 2022
+ * 
+ * Debug file
+ * You may want to keep this away from the public
+ */
+
+ use Tambula\Tambula;
+ require 'Tambula.php';
+ $tambula = new Tambula();
+
+// ---- YOUR CODE BELOW THIS LINE ---- //
+$tambula->setRequestUrl('/nl');
+$tambula->setDefaultRoute('https://default-route.local');
+$tambula->loadRoutesFromJson('routes.json');
+$tambula->setLanguage($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+$tambula->findCountryCode('41.210.146.199');
+
+// ---- OUR CODE BELOW THIS LINE ---- //
+?>
+<html>
+    <head>
+        <title>Tambula Debug Page</title>
+    </head>
+
+    <body>
+        <h1>Tambula Debug Page</h1>
+
+        <h2>findRoute()</h2>
+        <?php var_dump($tambula->findRoute()); ?>
+
+        <h2>Class Properties</h2>
+        <table border="1">
+            <tr>
+                <td><b>Property</b></td>
+                <td><b>Value</b></td>
+            </tr>
+        <?php 
+            $properties = ['defaultRoute', 'requestUrl', 'languageCode', 'geoPlugin', 'countryCode', 'requestPath', 'requestQuery', 'routes'];
+
+            foreach($properties as $property):
+        ?>
+            <tr>
+                <td><?=$property;?></td>
+                <td><pre><?php var_dump($tambula->$property);?></pre></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+
+    </body>
+
+</html>
+
+
